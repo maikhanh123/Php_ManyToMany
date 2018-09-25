0dd09de37b9beff2f7f104a74056291e71672528
Mai Khanh <maikhanh@Mais-MacBook-Pro.local>
2018-09-25 09:58:04 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,61 @@ Route::get('user/{id}/createRole', function ($id) {
 
 });
 
+Route::get('read', function () {
+    $users = \App\User::get();
+
+//   dd($users) ;
+    foreach ($users as $user) {
+        $name = $user->name;
+        $roleName = '';
+        foreach ($user->roles as $key => $role) {
+            if ($key > 0) {
+                $roleName .= ' & ' . $role->name;
+            } else {
+                $roleName .= $role->name;
+            }
+
+        }
+        echo $name . ' have role is: ' . $roleName . '</br>';
+    }
+});
+
+Route::get('update/{id}', function ($id) {
+     $user = \App\User::findOrFail($id);
+//     dd($user->roles);
+     foreach ($user->roles as $role) {
+//         dd ($role->pivot->role_id);
+         if($role->pivot->role_id == 2) {
+             $role->pivot->role_id = 1;
+             $role->pivot->save();
+         }
+     }
+});
+
+//attach more role for user
+Route::get('user/{userId}/attachRole/{roleId}', function ($userId, $roleId) {
+    $user = \App\User::findOrFail($userId);
+
+    $user->roles()->attach($roleId);
+
+});
+
+//Detach more role for user
+Route::get('user/{userId}/detachRole/{roleId}', function ($userId, $roleId) {
+    $user = \App\User::findOrFail($userId);
+
+    $user->roles()->detach($roleId);
+
+});
+
+//sync role for user
+Route::get('user/{userId}/syncRole/{roleId}', function ($userId, $roleId) {
+    $user = \App\User::findOrFail($userId);
+
+    $user->roles()->sync($roleId);
+
+});
+
+
+
 
